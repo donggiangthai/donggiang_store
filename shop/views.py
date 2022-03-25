79791b62635beaef030115596b223a5631b8b922
Đổng Giang Thái <donggiangthai1998@gmail.com>
2022-03-26 01:53:51 +0700

    render cart and editing cart

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from shop.models import Category, Product
+from cart.forms import CartAddProductForm
 
 
 # Create your views here.
@@ -23,10 +24,12 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, _id, slug):
     product = get_object_or_404(Product, id=_id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
     return render(
         request,
         'shop/product/detail.html',
         {
             'product': product,
+            'cart_product_form': cart_product_form,
         }
     )
