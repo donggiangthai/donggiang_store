64ff4126a00ae48ba336062c9451f4d3ab7112e5
Đổng Giang Thái <donggiangthai1998@gmail.com>
2022-03-26 01:01:12 +0700

    Create category and product in model, template, view

--- store/urls.py
+++ store/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls', namespace='shop')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^account/', include('allauth.urls')),
