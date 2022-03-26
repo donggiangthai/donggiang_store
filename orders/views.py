9a56a341ba26f020cc5043c218aa9820cbc74340
Đổng Giang Thái <donggiangthai1998@gmail.com>
2022-03-27 01:20:13 +0700

    Using celery for asynchronous send mail to client

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from orders.models import OrderItem
 from orders.forms import OrderCreateForm
 from cart.cart import Cart
+from orders.tasks import order_created
 
 
 # Create your views here.
@@ -20,6 +21,8 @@ def order_create(request):
                 )
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
---
+++ store/__init__.py
@@ -0,0 +1,1 @@
+from store.celery import app as celery_app
