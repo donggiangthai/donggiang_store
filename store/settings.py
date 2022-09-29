20c57697cc6d3b94f58de9a8c10f2bd0fce07d53
donggiangthai <donggiangthai1998@gmail.com>
2022-09-30 05:06:17 +0700

    revert wrong setting

--- store/settings.py
+++ store/settings.py
@@ -34,7 +34,7 @@ BRAINTREE_CONF = braintree.Configuration(
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
