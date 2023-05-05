05acba2117590796dc242682b1c2a95da984062e
Đổng Giang Thái <donggiangthai1998@gmail.com>
2023-05-06 05:53:39 +0700

    Correct the bash scripts to get the AWS SSM parameter store when the specific environment is empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
+if [[ -z $DATABASE_HOST ]] && [[ -z $DATABASE_PORT ]]; then
+  ssm_prefix='/donggiang_store'
+  if [[ -n $profile ]]; then
+    HOST_DB=$(source ssm-get-parameter.sh $ssm_prefix 'DATABASE_HOST' "$profile" | tr -d '"')
+    PORT_DB=$(source ssm-get-parameter.sh $ssm_prefix 'DATABASE_PORT' "$profile" | tr -d '"')
+  else
+    HOST_DB=$(source ssm-get-parameter.sh $ssm_prefix 'DATABASE_HOST' | tr -d '"')
+    PORT_DB=$(source ssm-get-parameter.sh $ssm_prefix 'DATABASE_PORT' | tr -d '"')
+  fi
+else
+  HOST_DB=$DATABASE_HOST
+  PORT_DB=$DATABASE_PORT
+fi
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+if [[ "$DATABASE" = "postgres" ]]; then
+  echo "Waiting for postgres..."
 
-    while ! nc -z "$HOST_DB" "$PORT_DB"; do
-      sleep 0.1
-    done
+  while ! nc -z "$HOST_DB" "$PORT_DB"; do
+    echo "Waiting for postgres: $HOST_DB:$PORT_DB"
+    sleep 1
+  done
 
-    echo "PostgreSQL started"
+  echo "PostgreSQL started"
 fi
 
 exec "$@"
--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
-# start-server.sh
 
-#source .env
+if [[ -z $DJANGO_SUPERUSER_EMAIL ]] && [[ -z $DJANGO_SUPERUSER_PASSWORD ]]; then
+  ssm_prefix='/donggiang_store'
+  if [[ -n $profile ]]; then
+    django_superuser_email=$(source ssm-get-parameter.sh $ssm_prefix 'DJANGO_SUPERUSER_EMAIL' "$profile" | tr -d '"')
+    django_superuser_password=$(source ssm-get-parameter.sh $ssm_prefix 'DJANGO_SUPERUSER_PASSWORD' "$profile" | tr -d '"')
+  else
+    django_superuser_email=$(source ssm-get-parameter.sh $ssm_prefix 'DJANGO_SUPERUSER_EMAIL' | tr -d '"')
+    django_superuser_password=$(source ssm-get-parameter.sh $ssm_prefix 'DJANGO_SUPERUSER_PASSWORD' | tr -d '"')
+  fi
+  export DJANGO_SUPERUSER_EMAIL=$django_superuser_email
+  export DJANGO_SUPERUSER_PASSWORD=$django_superuser_password
+fi
 
 # migration model to database
 make django-migration
@@ -10,7 +20,7 @@ make django-migration
 make celery-start
 
 if [[ -n "$DJANGO_SUPERUSER_EMAIL" ]] && [[ -n "$DJANGO_SUPERUSER_PASSWORD" ]]; then
-    python3 manage.py createsuperuser --no-input 2>&1
+  python3 manage.py createsuperuser --no-input 2>&1
 fi
 
 # Start Django server
