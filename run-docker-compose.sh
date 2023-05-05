c583e9e1c048591970643b528ac3e3a94e418870
Đổng Giang Thái <donggiangthai1998@gmail.com>
2023-05-06 06:06:41 +0700

    Correct run-docker-compose.sh (use for local only)
    Get arguments from the command line for flexibility using
    Handling the variable when not passing anything to the command line
    Separate the build image from the docker-compose up to use the flag --build-arg

--- run-docker-compose.sh
+++ run-docker-compose.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-declare -r env="$1"; shift
+# Get command-line arguments
+aws_profile_name="$1"
+shift
+env="$1"
+shift
+tag_key="$1"
+shift
 
+# Handle variable
+if [[ -z $aws_profile_name ]]; then
+  aws_profile_name='capstone'
+fi
+if [[ -z $tag_key ]]; then
+  tag_key='v1.0.0'
+fi
 docker_compose_file='docker-compose.yml'
 if [[ -n $env ]]; then
   docker_compose_file="docker-compose.$env.yml"
@@ -13,5 +26,9 @@ docker image prune --filter "dangling=true" --force
 # Down the compose
 #docker-compose down -v
 
+# Build the image
+docker-compose --file "$docker_compose_file" --project-name "donggiang_store_local" build \
+  --build-arg profile=$aws_profile_name
+
 # Run docker compose command
-docker-compose --file "$docker_compose_file" up --detach --build
+docker-compose --file "$docker_compose_file" --project-name "donggiang_store_local" up --detach --no-build
